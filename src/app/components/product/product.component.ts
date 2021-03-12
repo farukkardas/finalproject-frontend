5bfe00a3933f40b6978c8e1ca5220ab2b5eb4c49
gafar113 <falgamerss@gmail.com>
2021-03-12 18:11:42 +0300

    Kodlar refactor edildi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { ThisReceiver } from '@angular/compiler';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,10 +9,19 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded = false;
 
+  constructor(private productService: ProductService) {}
 
-  products:Product[] = [];
-  constructor() {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
-  ngOnInit(): void {}
+  getProducts() {
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
+  }
 }
